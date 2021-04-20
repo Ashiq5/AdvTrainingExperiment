804a6ff14b3d09599f79a175e591fb3f2f639181
ashiq <ishtiaqashiq5@gmail.com>
2021-04-20 17:26:28 -0400

    minor

--- AdversarialTraining/generate_adversarial_samples.py
+++ AdversarialTraining/generate_adversarial_samples.py
@@ -1,6 +1,6 @@
 from textattack.attack_results import SuccessfulAttackResult
 import textattack
-
+from tqdm import tqdm
 
 def _generate_adversarial_examples(model, args, dataset):
     """Create a dataset of adversarial examples based on perturbations of the
@@ -16,7 +16,8 @@ def _generate_adversarial_examples(model, args, dataset):
     adv_train_text, ground_truth_labels = [], []
     results_iterable = attack.attack_dataset(dataset)
     num_successes = 0
-    while num_successes < args.adversarial_samples_to_train:
+
+    for num_successes in tqdm(args.adversarial_samples_to_train):
         try:
             result = next(results_iterable)
             if isinstance(result, SuccessfulAttackResult):
